4970e7e838755f61b2ea8ae0bd7fa9730e9b3163
rtbrttny <b.ritz.design@gmail.com>
2017-07-09 14:58:19 -0400

    Start-up matchmaker assigment
    Styling has been updated

---
+++ Class-work/class07/first-javascript/script.js
@@ -0,0 +1,30 @@
+
+
+// alert('I am a popup and i am bad.');
+
+// document.write('Goodbye World');
+
+// var player1Score = 0
+
+// console.log(player1Score); 
+
+// player1Score = player1Score + 1 
+
+// console.log(player1Score);
+
+// player1Score = player1Score + 1 
+
+// console.log(player1Score);
+
+// console.log(document.getElementById('hello'));
+
+// console.log(document.querySelectorAll('.hello'));
+
+// document.querySelector('.lorem').style.color = 'red';
+
+function ourFirstFunction(){
+	console.log(1+3+5)
+	// code to excute
+}
+
+ourFirstFunction();
