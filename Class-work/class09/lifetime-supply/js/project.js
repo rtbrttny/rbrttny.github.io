f42831dfe693bc79b762933740029037b41f8303
rtbrttny <b.ritz.design@gmail.com>
2017-07-16 16:19:48 -0400

    hw updates
    Project proposal is uploaded.
    
    I attempted to make some changes in the HW due to your feedback but I may have made it worse.  I keep on getting errors in the console however I tried to place the whole formula within the containers.
    
    I know we dont need to use concantation, however if I already have it labeled there and its working would it be ok to leave it?
    
    looking forward to the soultion review this Monday.
    
    Best,
    Brittany

--- Class-work/class09/lifetime-supply/js/project.js
+++ Class-work/class09/lifetime-supply/js/project.js
@@ -2,23 +2,45 @@
 	$('.click-me').click(submitClick);
 
 function submitClick () {
+	// $('.solution').html('You Will Drink' + total + 'more' + item + 'in Your Lifetime')
 	  var age = $('#age').val();
 	  var maxiumAge = $('#max-age').val();
 	  var item = $('#item').val();
 	  var dayAmount = $('#num-per-day').val();
-	}
+
+
+
+	  $(age.val() * maxiumAge.val() * dayAmount.val().click(function(){
+		total = 0;
+	$('.final').html(submitClick);
+// });
+
+// 	  $('#age'.val() * '#max-age'.val() * '#num-per-day'.val().click(function(){
+// 		total = 0;
+// 	$('.final').html(total);
+// });
+
+	  $('.solution').html(function){
+	  	$('You Will Drink' + total + 'more' + item + 'in Your Lifetime')
+
+	});
 
 // $('#solution').html('You will drink' 
 // 	+ (age * max-age) * (item * num-per-day) + 'inYourLifetime'}
 
 
-$('#age'.val() * '#max-age'.val() * '#num-per-day'.val()).click(function(){
-	total = 0;
-	$('.solution').html(total);
-});
 
-$('.solution').html('You Will Drink' + total + 'more' + item + 'in Your Lifetime')}
+// $('#out').click(function(){
+// 	$('#out').css('background-color', 'white');
+// 	total -= 0; 
+// });
 
+
+
+// $('#a30').click(function(){
+// 	total += 30;
+// 	$('#out').html(total);
+// });
 
 
 // HTML
