e1d5cdb2dec7e68fc04a2d732c035ef066b63ec7
rtbrttny <b.ritz.design@gmail.com>
2017-08-21 18:28:04 -0400

    updated

--- Final-project/Final Project_Thesis 05/js/app.js
+++ Final-project/Final Project_Thesis 05/js/app.js
@@ -6,6 +6,9 @@
 // 	$('.iso-line').removeClass('iso-line--active');
 // });
 console.log('javascript loaded');
+
+var $isoLineLeft =  $('.iso-line-left');
+
 $('.circle-wrap').hover(function(event) { // this is what happens on mouseenter
 	// console.log('circle got hovered!', event.target);
 	// pick out the x and y positions
@@ -62,44 +65,66 @@ $('.circle-wrap').hover(function(event) { // this is what happens on mouseenter
 	}
 
 	// grab the class that contains the column
-	var letter = event.target.children[0].classList[1];
+	var letter = event.target.children[0] ? event.target.children[0].classList[1] : '';
 	// check for rows and hover
 	if(letter == 'a' || letter == 'b') {
 		// console.log('this worked')
 		$('.column9').addClass('colortemp--active');
 		// debugger
 
+		$isoLineLeft.css('width', '500px');
+
 	} else if(letter == 'c' || letter =='d'){
 		$('.column8').addClass('colortemp--active');
+
+		$isoLineLeft.css('width', '400px');
 
 	} else if(letter == 'e' || letter =='f'){
 		$('.column7').addClass('colortemp--active');
 
+		$isoLineLeft.css('width', '350px');
+
 	} else if(letter == 'g' || letter =='h'){
 		$('.column6').addClass('colortemp--active');
+
+		$isoLineLeft.css('width', '300px');
 
 	} else if(letter == 'i' || letter =='j' ){
 		$('.column5').addClass('colortemp--active');
 
+		$isoLineLeft.css('width', '250px');
+
 	} else if(letter == 'k' || letter =='l'){
 		$('.column4').addClass('colortemp--active');
+
+		$isoLineLeft.css('width', '300px');
 
 	} else if(letter == 'm' || letter == 'n' ){
 		$('.column3').addClass('colortemp--active');
 
+		$isoLineLeft.css('width', '350px');
+
 	} else if(letter == 'o' || letter == 'p' ){
 		$('.column2').addClass('colortemp--active');
 
+		$isoLineLeft.css('width', '400px');
+
 	} else if(letter == 'q' || letter == 'r' ){
 		$('.column1').addClass('colortemp--active');
+
+		$isoLineLeft.css('width', '500px');
 
 	}
 
 
 
 }, function(event) { // this is what happens on mouseleave
-	var selectedClass2 = event.target.parentElement.classList[2];
+	var selectedClass2 = event.target.parentElement ? event.target.parentElement.classList[2] : '';
 	// console.log('the selected class to remove' , selectedClass2);
+
+
+	$isoLineLeft.hide()
+
 	if(selectedClass2 == 'row1' || selectedClass2 == 'row2') {
 		$('.line1').removeClass('iso-line--active');
 
@@ -130,7 +155,8 @@ $('.circle-wrap').hover(function(event) { // this is what happens on mouseenter
 
 	}
 
-	var letter = event.target.children[0].classList[1];
+	var letter = event.target.children[0] ?  event.target.children[0].classList[1] : 0;
+	
 	if(letter == 'a' || letter == 'b') {
 		// console.log('this worked')
 		$('.column9').removeClass('colortemp--active');
@@ -165,66 +191,20 @@ $('.circle-wrap').hover(function(event) { // this is what happens on mouseenter
 });
 
 
-// $('.circle-wrap').hover(function(event) { // this is what happens on mouseenter
-// 	console.log('circle got hovered!', event.target);
-// 	// pick out the x and y positions
-// 	debugger
-// 	var selectedClass = event.target.parentElement.parentElement.classList[2];
-// 	console.log(selectedClass);
-// 	if(selectedClass == 'a' || selectedClass == 'b') {
-// 		$('.column1').addClass('colortemp--active');
+// console.log($isoLineLeft.length)
 
-// 	} else if(selectedClass == 'c' || selectedClass =='d'){
-// 		$('.column2').addClass('colortemp--active');
+// $('.circle-wrap').mousemove(function(){
+// 	if ($(this).children('.a').length > 0) {
+// 	 console.log('a');
 
-// 	} else if(selectedClass == 'e' selectedClass =='f'){
-// 		$('.column3').addClass('colortemp--active');
+// 	$isoLineLeft.css('width', '434px');
+// 	} else if ($(this).children('.b').length > 0) {
+// 	 console.log('b')
 
-// 	} else if(selectedClass == 'g' selectedClass =='h'){
-// 		$('.column4').addClass('colortemp--active');
-
-// 	} else if(selectedClass == 'i' selectedClass =='j' ){
-// 		$('.column5').addClass('colortemp--active');
-
-// 	} else if(selectedClass == 'k' electedClass =='l'){
-// 		$('.column6').addClass('colortemp--active');
-
-// 	} else if(selectedClass == 'm' || selectedClass == 'n' ){
-// 		$('.column7').addClass('colortemp--active');
-
-// 	} else if(selectedClass == 'o' || selectedClass == 'p' ){
-// 		$('.column8').addClass('colortemp--active');
-
-// 	} else if(selectedClass == 'q' || selectedClass == 'r' ){
-// 		$('.column9').addClass('colortemp--active');
-
+// 	 $isoLineLeft.css('width', '300px');
+// 	} else {
+// 	  $isoLineLeft.css('width', '200px');
 // 	}
 
-// }, function(event) { // this is what happens on mouseleave
-// 	var selectedClass = event.target.parentElement.parentElement.classList[2];
-// 	console.log(selectedClass);
-// 	if(selectedClass == 'row1' || selectedClass == 'row2') {
 
-// 		$('.line1').removeClass('iso-line--active');
-// 	} else if(selectedClass == 'row3' || selectedClass =='row4'){
-// 		$('.line2').removeClass('iso-line--active');
-
-// 	} else if(selectedClass == 'row5' ){
-// 		$('.line5').removeClass('iso-line--active');
-
-// 	} else if(selectedClass == 'row6' ){
-// 		$('.line5').removeClass('iso-line--active');
-
-// 	} else if(selectedClass == 'row7' ){
-// 		$('.line6').removeClass('iso-line--active');
-
-// 	} else if(selectedClass == 'row8' ){
-// 		$('.line7').removeClass('iso-line--active');
-		
-// 	} else if(selectedClass == 'row9' || selectedClass == 'row10' ){
-// 		$('.line8').removeClass('iso-line--active');
-
-// 	} else if(selectedClass == 'row11' || selectedClass == 'row12' ){
-// 		$('.line9').removeClass('iso-line--active');
-
-// 	}
+// });
